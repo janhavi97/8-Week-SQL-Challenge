a1828a3826915502fc13157564f07872b6da45de
Janhavi Patil <janhavipatil80@gmail.com>
2024-07-02 16:08:41 -0500

    Update Danny's Diner.sql

--- Case Study #1 - Danny's Diner/Danny's Diner.sql
+++ Case Study #1 - Danny's Diner/Danny's Diner.sql
@@ -5,14 +5,20 @@ JOIN dannys_diner.menu m
 ON s.product_id = m.product_id
 GROUP BY s.customer_id
 ORDER BY s.customer_id;
+-- Customer A spent $76.
+-- Customer B spent $74.
+-- Customer C spent $36.
 
-    
+
 -- 2. How many days has each customer visited the restaurant?
 SELECT s.customer_id, COUNT(DISTINCT s.order_date) as visit_count
 FROM dannys_diner.sales s
 GROUP BY s.customer_id;
+-- Customer A visited 4 times.
+-- Customer B visited 6 times.
+-- Customer C visited 2 times.
 
-    
+
 -- 3. What was the first item from the menu purchased by each customer?
 SELECT a.customer_id, a.product_name
 FROM
@@ -24,6 +30,9 @@ FROM
         GROUP BY s.customer_id, m.product_name, s.order_date
     ) a
 WHERE a.RNK1 = 1;
+-- Customer A's first order is curry and sushi.
+-- Customer B's first order is curry.
+-- Customer C's first order is ramen.
 
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
@@ -34,9 +43,22 @@ ON s.product_id = m.product_id
 GROUP BY m.product_name
 ORDER BY SUM(s.product_id) DESC
 LIMIT 1;
+-- Most purchased item on the menu is ramen which was purchased 8 times
 
 
 -- 5. Which item was the most popular for each customer?
+SELECT a.customer_id, a.product_name, a.product_count
+FROM
+    (
+        SELECT s.customer_id, m.product_name, COUNT(m.product_name) as product_count, RANK() OVER(PARTITION BY s.customer_id ORDER BY COUNT(m.product_name) DESC) AS RNK1
+        FROM dannys_diner.sales s
+        JOIN dannys_diner.menu m
+        ON s.product_id = m.product_id
+        GROUP BY s.customer_id, m.product_name
+    ) a
+WHERE a.RNK1 = 1;
+-- Customer A and Customer C's preferred item is ramen.
+-- Customer B enjoys every item on the menu.
 
 
 -- 6. Which item was purchased first by the customer after they became a member?
