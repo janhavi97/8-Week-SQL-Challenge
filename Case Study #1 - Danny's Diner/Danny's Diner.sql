e0fa764712618dd20be441f855483e0990e8ef28
Janhavi Patil <janhavipatil80@gmail.com>
2024-07-02 15:49:37 -0500

    Update Danny's Diner.sql

--- Case Study #1 - Danny's Diner/Danny's Diner.sql
+++ Case Study #1 - Danny's Diner/Danny's Diner.sql
@@ -1,91 +1,56 @@
 -- 1. What is the total amount each customer spent at the restaurant?
-SELECT s.customer_id, SUM(p.price)
+SELECT s.customer_id, SUM(m.price) as total_spent
 FROM dannys_diner.sales s
-JOIN dannys_diner.menu p
-ON s.product_id = p.product_id
+JOIN dannys_diner.menu m
+ON s.product_id = m.product_id
 GROUP BY s.customer_id
+ORDER BY s.customer_id;
 
-**Schema (PostgreSQL v13)**
-
-    CREATE SCHEMA dannys_diner;
-    SET search_path = dannys_diner;
     
-    CREATE TABLE sales (
-      "customer_id" VARCHAR(1),
-      "order_date" DATE,
-      "product_id" INTEGER
-    );
-    
-    INSERT INTO sales
-      ("customer_id", "order_date", "product_id")
-    VALUES
-      ('A', '2021-01-01', '1'),
-      ('A', '2021-01-01', '2'),
-      ('A', '2021-01-07', '2'),
-      ('A', '2021-01-10', '3'),
-      ('A', '2021-01-11', '3'),
-      ('A', '2021-01-11', '3'),
-      ('B', '2021-01-01', '2'),
-      ('B', '2021-01-02', '2'),
-      ('B', '2021-01-04', '1'),
-      ('B', '2021-01-11', '1'),
-      ('B', '2021-01-16', '3'),
-      ('B', '2021-02-01', '3'),
-      ('C', '2021-01-01', '3'),
-      ('C', '2021-01-01', '3'),
-      ('C', '2021-01-07', '3');
-     
-    
-    CREATE TABLE menu (
-      "product_id" INTEGER,
-      "product_name" VARCHAR(5),
-      "price" INTEGER
-    );
-    
-    INSERT INTO menu
-      ("product_id", "product_name", "price")
-    VALUES
-      ('1', 'sushi', '10'),
-      ('2', 'curry', '15'),
-      ('3', 'ramen', '12');
-      
-    
-    CREATE TABLE members (
-      "customer_id" VARCHAR(1),
-      "join_date" DATE
-    );
-    
-    INSERT INTO members
-      ("customer_id", "join_date")
-    VALUES
-      ('A', '2021-01-07'),
-      ('B', '2021-01-09');
-
----
-
-**Query #1**
-
-    SELECT s.customer_id, SUM(p.price)
-    FROM dannys_diner.sales s
-    JOIN dannys_diner.menu p
-    ON s.product_id = p.product_id
-    GROUP BY s.customer_id;
-
-| customer_id | sum |
-| ----------- | --- |
-| B           | 74  |
-| C           | 36  |
-| A           | 76  |
-
----
-
-[View on DB Fiddle](https://www.db-fiddle.com/f/2rM8RAnq7h5LLDTzZiRWcd/138)
 -- 2. How many days has each customer visited the restaurant?
+SELECT s.customer_id, COUNT(DISTINCT s.order_date) as visit_count
+FROM dannys_diner.sales s
+GROUP BY s.customer_id;
+
+    
 -- 3. What was the first item from the menu purchased by each customer?
+SELECT a.customer_id, a.product_name
+FROM
+    (
+        SELECT s.customer_id, m.product_name, RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date) AS RNK1
+        FROM dannys_diner.sales s
+        JOIN dannys_diner.menu m
+        ON s.product_id = m.product_id
+        GROUP BY s.customer_id, m.product_name, s.order_date
+    ) a
+WHERE a.RNK1 = 1;
+
+
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+SELECT m.product_name, COUNT(s.product_id) as no_of_time_purchased
+FROM dannys_diner.sales s
+JOIN dannys_diner.menu m
+ON s.product_id = m.product_id
+GROUP BY m.product_name
+ORDER BY SUM(s.product_id) DESC
+LIMIT 1;
+
+
 -- 5. Which item was the most popular for each customer?
+
+
 -- 6. Which item was purchased first by the customer after they became a member?
+
+
 -- 7. Which item was purchased just before the customer became a member?
+
+
 -- 8. What is the total items and amount spent for each member before they became a member?
+
+
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
+
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+
+
