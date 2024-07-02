07c7536e4380fe33fc4390c2061b30f54c2e7de3
Janhavi Patil <janhavipatil80@gmail.com>
2024-07-02 16:46:23 -0500

    Update Danny's Diner.sql

--- Case Study #1 - Danny's Diner/Danny's Diner.sql
+++ Case Study #1 - Danny's Diner/Danny's Diner.sql
@@ -1,8 +1,8 @@
 -- 1. What is the total amount each customer spent at the restaurant?
-SELECT s.customer_id, SUM(m.price) as total_spent
+SELECT s.customer_id, SUM(p.price) AS total_spent
 FROM dannys_diner.sales s
-JOIN dannys_diner.menu m
-ON s.product_id = m.product_id
+JOIN dannys_diner.menu p
+ON s.product_id = p.product_id
 GROUP BY s.customer_id
 ORDER BY s.customer_id;
 -- Customer A spent $76.
@@ -11,7 +11,7 @@ ORDER BY s.customer_id;
 
 
 -- 2. How many days has each customer visited the restaurant?
-SELECT s.customer_id, COUNT(DISTINCT s.order_date) as visit_count
+SELECT s.customer_id, COUNT(DISTINCT s.order_date) AS visit_count
 FROM dannys_diner.sales s
 GROUP BY s.customer_id;
 -- Customer A visited 4 times.
@@ -20,28 +20,28 @@ GROUP BY s.customer_id;
 
 
 -- 3. What was the first item from the menu purchased by each customer?
-SELECT a.customer_id, a.product_name
-FROM
-    (
-        SELECT s.customer_id, m.product_name, RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date) AS RNK1
-        FROM dannys_diner.sales s
-        JOIN dannys_diner.menu m
-        ON s.product_id = m.product_id
-        GROUP BY s.customer_id, m.product_name, s.order_date
-    ) a
-WHERE a.RNK1 = 1;
+SELECT s.customer_id, p.product_name
+FROM dannys_diner.sales s
+JOIN dannys_diner.menu p 
+ON s.product_id = p.product_id
+WHERE (s.customer_id, s.order_date) IN (
+                                            SELECT customer_id, MIN(order_date)
+                                            FROM dannys_diner.sales
+                                            GROUP BY customer_id
+                                       )
+ORDER BY s.customer_id;
 -- Customer A's first order is curry and sushi.
 -- Customer B's first order is curry.
 -- Customer C's first order is ramen.
 
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
-SELECT m.product_name, COUNT(s.product_id) as no_of_time_purchased
+SELECT p.product_name, COUNT(*) AS no_of_time_purchased
 FROM dannys_diner.sales s
-JOIN dannys_diner.menu m
-ON s.product_id = m.product_id
-GROUP BY m.product_name
-ORDER BY SUM(s.product_id) DESC
+JOIN dannys_diner.menu p 
+ON s.product_id = p.product_id
+GROUP BY p.product_name
+ORDER BY no_of_time_purchased DESC
 LIMIT 1;
 -- Most purchased item on the menu is ramen which was purchased 8 times
 
@@ -50,11 +50,11 @@ LIMIT 1;
 SELECT a.customer_id, a.product_name, a.product_count
 FROM
     (
-        SELECT s.customer_id, m.product_name, COUNT(m.product_name) as product_count, RANK() OVER(PARTITION BY s.customer_id ORDER BY COUNT(m.product_name) DESC) AS RNK1
+        SELECT s.customer_id, p.product_name, COUNT(p.product_name) AS product_count, RANK() OVER(PARTITION BY s.customer_id ORDER BY COUNT(p.product_name) DESC) AS RNK1
         FROM dannys_diner.sales s
-        JOIN dannys_diner.menu m
-        ON s.product_id = m.product_id
-        GROUP BY s.customer_id, m.product_name
+        JOIN dannys_diner.menu p
+        ON s.product_id = p.product_id
+        GROUP BY s.customer_id, p.product_name
     ) a
 WHERE a.RNK1 = 1;
 -- Customer A and Customer C's preferred item is ramen.
@@ -62,15 +62,63 @@ WHERE a.RNK1 = 1;
 
 
 -- 6. Which item was purchased first by the customer after they became a member?
+SELECT customer_id, product_name
+FROM
+    (
+        SELECT s.customer_id, p.product_name, RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date) AS RNK1
+        FROM dannys_diner.sales s
+        JOIN dannys_diner.menu p
+        ON s.product_id = p.product_id
+      	JOIN dannys_diner.members m
+      	ON s.customer_id = m.customer_id
+      	WHERE m.join_date <= s.order_date 
+    ) a
+WHERE RNK1 = 1;
+-- Customer A's first order as a member is ramen.
+-- Customer B's first order as a member is sushi.
 
-
+    
 -- 7. Which item was purchased just before the customer became a member?
+SELECT customer_id, product_name
+FROM
+    (
+        SELECT s.customer_id, p.product_name, RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date DESC) AS RNK1
+        FROM dannys_diner.sales s
+        JOIN dannys_diner.menu p
+        ON s.product_id = p.product_id
+      	JOIN dannys_diner.members m
+      	ON s.customer_id = m.customer_id
+      	WHERE m.join_date > s.order_date 
+    ) a
+WHERE RNK1 = 1;
+-- Customer A's order just before becoming a member is sushi and curry.
+-- Customer B's order just before becoming a member is sushi.
 
 
 -- 8. What is the total items and amount spent for each member before they became a member?
+SELECT s.customer_id, COUNT(s.product_id) AS product_count, SUM(p.price) AS amount_spent
+FROM dannys_diner.sales s
+JOIN dannys_diner.menu p
+ON s.product_id = p.product_id
+JOIN dannys_diner.members m
+ON s.customer_id = m.customer_id
+WHERE m.join_date > s.order_date 
+GROUP BY s.customer_id
+ORDER BY s.customer_id
+-- Customer A spent $25 on 2 items.
+-- Customer B spent $40 on 3 items.
 
 
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+SELECT s.customer_id, SUM(CASE WHEN p.product_name = 'sushi' THEN p.price * 20 ELSE p.price * 10 END) AS total_points
+FROM dannys_diner.sales s
+JOIN dannys_diner.menu p 
+    ON s.product_id = p.product_id
+GROUP BY s.customer_id
+ORDER BY s.customer_id;
+-- Customer A has 860 points
+-- Customer B has 940 points
+-- Customer C has 360 points  
 
 
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
