d0c221c62946d696735a2050cdfe779d01eb1fe9
Janhavi Patil <janhavipatil80@gmail.com>
2024-07-06 21:46:18 -0500

    Update Runner and Customer Experience.sql

--- Case Study #2 - Pizza Runner/Runner and Customer Experience.sql
+++ Case Study #2 - Pizza Runner/Runner and Customer Experience.sql
@@ -1,5 +1,12 @@
 -- B. Runner and Customer Experience
 -- How many runners signed up for each 1 week period? (i.e. week starts 2020-01-01)
+SELECT FLOOR((EXTRACT(DAY FROM age(registration_date, '2021-01-01')) / 7)) AS registration_week,
+       COUNT(runner_id) AS runner_signup
+FROM pizza_runner.runners
+GROUP BY registration_week
+ORDER BY registration_week;
+-- In Week 1, 2 new runners signed up.
+-- In Week 2 and 3, 1 new runner signed up.
 
 
 -- What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
